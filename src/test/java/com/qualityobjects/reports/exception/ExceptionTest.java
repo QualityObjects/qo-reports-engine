f256edc9a11296c0e3398567a6086cfbac0e6ee9
Alfredo Ferrero <aferrero@qualityobjects.com>
2021-03-01 13:53:53 +0100

    Cambio import exception y version pom

--- src/test/java/com/qualityobjects/reports/exception/ExceptionTest.java
+++ src/test/java/com/qualityobjects/reports/exception/ExceptionTest.java
@@ -1,6 +1,5 @@
 package com.qualityobjects.reports.exception;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
